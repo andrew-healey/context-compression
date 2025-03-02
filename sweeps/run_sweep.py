ce755775e01a87dc4558981cf878002a03454cab
Andrew Healey (machine account) <doolie.healey+machine-account@gmail.com>
2025-03-02 01:53:56 +0000

    configurable sweep file name

--- sweeps/run_sweep.py
+++ sweeps/run_sweep.py
@@ -12,7 +12,17 @@ def main():
     os.environ["WORLD_SIZE"] = str(dist.get_world_size())
     
     # Run the bash script
-    subprocess.run(["bash", "sweeps/sweep_proxy_model.sh"], check=True)
+    # Get the script filename from command line arguments
+    if len(sys.argv) < 2:
+        print("Error: Please provide a script filename as an argument")
+        sys.exit(1)
+    
+    script_path = sys.argv[1]
+    
+    # Assert that the script file exists
+    assert os.path.exists(script_path), f"Script file '{script_path}' does not exist"
+    
+    subprocess.run(["bash", script_path], check=True)
 
 if __name__ == "__main__":
     main() 
