efbee9a3fd8b99ee5b2cc15183d26cbebbb11ebe
Andrew Healey (machine account) <doolie.healey+machine-account@gmail.com>
2025-03-03 02:50:31 +0000

    add results of run #2

---
+++ sweeps/sweep_wider_is_better.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# ok so we have enough vram per gpu to fit two concurrent runs on it.
+# so we'll do that.
+# so the CUDA_VISIBLE_DEVICES=floor($RANK/2)
+
+if [ -z "$RANK" ] || [ -z "$WORLD_SIZE" ]; then
+  echo "RANK and WORLD_SIZE must be defined"
+  exit 1
+fi
+
+i=0
+for n_heads in 2 4; do
+    for attention_kind in selective; do
+        for lr in 1.5e-5 2e-5 3e-5; do
+            for total_batch_size in 10240; do
+                batch_size=$((total_batch_size / 256))
+                for seed in 1338 1339 1340 1341; do
+                    i=$((i + 1))
+                    if [ $((i % $WORLD_SIZE)) -ne $RANK ]; then
+                        continue
+                    fi
+                    out_dir="wider_is_better_7/attention_kind${attention_kind}_n_heads${n_heads}_seed${seed}"
+                    # Use CUDA_DEVICE=0 since after setting CUDA_VISIBLE_DEVICES, the only visible device is 0
+                    HALF_RANK=$((RANK % 8))
+                    { CUDA_VISIBLE_DEVICES=$HALF_RANK WORLD_SIZE=1 LOCAL_RANK=0 RANK=0 python -m context_compression.train --group wider_is_better_7 \
+                    --log_dir $out_dir \
+                    --max_lr $lr \
+                    --total_batch_size $total_batch_size \
+                    --seq_len 256 \
+                    --max_steps 50000 \
+                    --warmup_steps 200 \
+                    --batch_size $batch_size \
+                    --mup \
+                    --n_heads $n_heads \
+                    --key ${lr}_${total_batch_size}_${n_heads}_${seed} $([ "$attention_kind" = "selective" ] || echo "--disable_selection") \
+                    --random_seed $seed; }
+                done
+            done
+        done
+    done
+done
+
+# result number 0: [See here](https://wandb.ai/sesamestrong/context_compression?nw=pe07md4nhw).
+
+# For selection=false, wider *IS* better.
+# But for selection=true, wider is worse.
+
+# selection=false seems to ALWAYS be worse than selection=true!!
+# I think this means my training is very selection-unfriendly.
+# And definitely not representative of the real benefits of selection.
+
+# result number 1 (letting all of them run for longer):
+# TODO finish this!!!
+
+echo 'done'
+
+# result number 2 (selective, total bs=10240):
+# we def have wider-is-better - nice! but not with perfect lr mup transfer.
+# for n_heads=2, lr=5e-5 is best
+# for n_heads=4, lr=3e-5 is best
+# for n_heads=12, lr=2e-5 is better than lr=5e-5. not sure abt the resolution.
+
+# but we gotta improve efficiency - increase total bs as much as I can.
+# will try doing it for n_heads=4 rn, hopefully those best bs's can transfer to n_heads=12.
