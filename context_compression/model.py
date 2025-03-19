fix incorrect head_dim computation

context_compression/model.py
@@ -111,9 +111,9 @@ class GPTConfig:
     def __post_init__(self):
         if self.attn_mult is None:
             if self.mup:
-                self.attn_mult = (self.n_embd / self.n_head)**-1.0
+                self.attn_mult = (self.head_dim)**-1.0
             else:
-                self.attn_mult = (self.n_embd / self.n_head)**-0.5
+                self.attn_mult = (self.head_dim)**-0.5
 
 class GPT(nn.Module):
     def __init__(self, config):